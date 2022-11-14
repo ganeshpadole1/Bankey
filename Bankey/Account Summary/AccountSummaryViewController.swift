eb84d362203189c151d76fc6a9f439d599471a67
Ganesh Padole <ganeshpadole55@gmail.com>
2022-11-14 17:17:52 +0530

    added account header view

--- Bankey/Account Summary/AccountSummaryViewController.swift
+++ Bankey/Account Summary/AccountSummaryViewController.swift
@@ -25,6 +25,7 @@ class AccountSummaryViewController: UIViewController {
 
 extension AccountSummaryViewController {
     private func setup() {
+        setupTableHeaderView()
         setupTableView()
     }
 
@@ -41,6 +42,16 @@ extension AccountSummaryViewController {
             tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
             tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
         ])
+    }
+
+    func setupTableHeaderView() {
+        let header = AccountSummaryHeaderView(frame: .zero)
+
+        var size = header.systemLayoutSizeFitting(UIView.layoutFittingCompressedSize)
+        size.width = UIScreen.main.bounds.width
+        header.frame.size = size
+
+        tableView.tableHeaderView = header
     }
 }
 
---
+++ Bankey/Account Summary/Header/AccountSummaryHeaderView.swift
@@ -0,0 +1,40 @@
+//
+//  AccountSummaryHeader.swift
+//  Bankey
+//
+//  Created by Ganesh on 13/11/22.
+//
+
+import UIKit
+
+class AccountSummaryHeaderView: UIView {
+
+    @IBOutlet weak var contentView: UIView!
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+
+    override var intrinsicContentSize: CGSize {
+        return CGSize(width: UIView.noIntrinsicMetric, height: 144)
+    }
+
+    private func commonInit() {
+        let bundle = Bundle(for: AccountSummaryHeaderView.self)
+        bundle.loadNibNamed("AccountSummaryHeaderView", owner: self, options: nil)
+        addSubview(contentView)
+        contentView.backgroundColor = appColor
+
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        contentView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        contentView.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
+        contentView.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+        contentView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+    }
+}
